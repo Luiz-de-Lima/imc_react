c4e6a24d775593c521223a69f8cee9fd75451bdd
Luiz-de-Lima <luiz12359@gmail.com>
2022-07-02 02:12:36 -0300

    criando o leftSide

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./App.module.css";
 import logo from "../src/assets/powered.png";
 
 const App = () => {
+  const [heightField, setHeightField] = useState(0);
+  const [weightField, setWeightField] = useState<number>(0);
+  type InputChangeElement = React.ChangeEvent<HTMLInputElement>;
+
+  const teste = (event: InputChangeElement) => {
+    const value = parseFloat(event.target.value);
+    if (value < 1) {
+      alert("números negativos não se encaixam em altura");
+    }
+    setHeightField(value);
+  };
+
+  const handleCalculateButton=()=>{
+    if(heightField && weightField){
+
+    }else{
+      alert('existem campos vazios, por favor preencher para prosseguir')
+    }
+  }
+
   return (
     <div className={styles.main}>
       <header>
@@ -10,8 +30,30 @@ const App = () => {
           <img src={logo} alt="" width={150} />
         </div>
       </header>
-      <div className={styles.Container}>
-        <div className={styles.lefSide}>...lado</div>
+      <div className={styles.container}>
+        <div className={styles.leftSide}>
+          <h1>Calcule o seu IMC</h1>
+
+          <p>
+            IMC é a sigla para Índice de Massa Corpórea, parâmetro adotado pela
+            Organização Mundial de Saúde para calcular o peso ideal de cada
+            pessoa.
+          </p>
+
+          <input
+            type="number"
+            placeholder="digite a sua altura. Ex:1.5 (em métros)"
+            value={heightField > 0 ? heightField : ""}
+            onChange={teste}
+          />
+          <input
+            type="number"
+            placeholder="digite o seu peso. Ex:30.5 (em kg)"
+            value={weightField > 0 ? weightField : ""}
+            onChange={teste}
+          />
+          <button onClick={handleCalculateButton}>Calcular</button>
+        </div>
         <div className={styles.rightSide}>...lado</div>
       </div>
     </div>
